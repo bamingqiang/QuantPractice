5c266e05d54d218655e44117770311f2d90f3762
bamingqiang <bamingqiang@163.com>
2020-03-14 07:21:49 +0800

    币币账户，增加杠杆交易功能，包括：借币、还币，以及6种交易情况。但仍存在问题：1、6种交易情况的判断应该采用交易信号 + 仓位信息；2、还需增加各环节的容错机制，包括借币、还币、下单、取数据等环节。

--- functions.py
+++ functions.py
@@ -1,15 +1,17 @@
 import pandas as pd
 from datetime import datetime, timedelta
+from math import ceil
 import time
+import message as msg
 
 
-def next_run_time(time_interval, ahead_time=1):
+def next_run_time(time_interval, ahead_time=1, dif_minute=0):
     """计算最近一次获取交易K线数据时间点（邢大编写）
 
     :argument
         time_interval: string, 获取数据频次间隔时间，以“数字 + m”形式，表示多少分钟，例：15m
         ahead_time: 预留的空余时间以做好准备：若现在距离目标时间太短（小于ahead_time + 1秒）则跳过一次
-
+        dif_minute: 与整倍的time_interval时间错开的时间分钟数
     :return
         target_time: 最近一次获取交易所K线数据计算交易信号的时间，与现在时间至少间隔两秒；
 
@@ -53,20 +55,25 @@ def get_candle_data(exchange, time_interval, symbol, end_time):
             'volume': float
         None: 数据不合格时返回
     """
-    try:
-        df = pd.DataFrame(exchange.fetch_ohlcv(symbol, time_interval, since=0), dtype=float)
-        df.rename(columns={0: 'UTC', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'}, inplace=True)
-        df['candle_begin_time_GMT8'] = pd.to_datetime(df['UTC'], unit='ms') + timedelta(hours=8)
-        df = df[['candle_begin_time_GMT8', 'open', 'high', 'low', 'close', 'volume']]
-        # 验证数据是否包含最新一根K线，以及是否包含多余一根K线（K线时间未完成）
-        if (df.iat[-1, 0] + timedelta(minutes=int(time_interval.strip('m')))) > end_time:
-            df = df[0:-1]  # 去除最后一根多余K线
-        if (df.iat[-1, 0] + timedelta(minutes=int(time_interval.strip('m')))) != end_time:
-            return  # 最后一根K线不是最近一根，获取的不是最新数据，返回None
-        return df
-    except Exception as e:
-        print('Can not get data.')
-        return
+    for i in range(10):
+        try:
+            df = pd.DataFrame(exchange.fetch_ohlcv(symbol, time_interval, since=0), dtype=float)
+            df.rename(columns={0: 'UTC', 1: 'open', 2: 'high', 3: 'low', 4: 'close', 5: 'volume'}, inplace=True)
+            df['candle_begin_time_GMT8'] = pd.to_datetime(df['UTC'], unit='ms') + timedelta(hours=8)
+            df = df[['candle_begin_time_GMT8', 'open', 'high', 'low', 'close', 'volume']]
+            # 验证数据是否包含最新一根K线，以及是否包含多余一根K线（K线时间未完成）
+            if (df.iat[-1, 0] + timedelta(minutes=int(time_interval.strip('m')))) > end_time:
+                df = df[0:-1]  # 去除最后一根多余K线
+            if (df.iat[-1, 0] + timedelta(minutes=int(time_interval.strip('m')))) != end_time:
+                return  # 最后一根K线不是最近一根，获取的不是最新数据，返回None
+            return df
+        except Exception as e:
+            if i == 9:
+                content = '获取K线数据失败达到10次，重新开始循环！\n'
+                content += '退出的本次时间：' + datetime.strftime(end_time, '%Y-%m-%d %H:%M:%S')
+                msg.send_dingding_msg(content)
+                print(e)
+                return
 
 
 def get_brin_signal(candle_data, period_interval=100, std_times=2):
@@ -95,8 +102,7 @@ def get_brin_signal(candle_data, period_interval=100, std_times=2):
     signal_trade = {1.0: 'long', -1.0: 'short', 0.0: 'closing', 9: 'nothing'}
 
     if len(candle_data) < period_interval:
-        # raise ('K线数据量不足，无法计算买卖信号。')
-        print('K线数据量不足，无法计算买卖信号。')
+        # print('K线数据量不足，无法计算买卖信号。')
         return 'nothing'
 
     # 1. 算出三条线, upper/middle/lower
@@ -107,7 +113,6 @@ def get_brin_signal(candle_data, period_interval=100, std_times=2):
     candle_data['lower'] = candle_data['middle'] - candle_data['std'] * std_times
 
     # 2. 确定多空平信号，只需要确定最后一根K线是否产生信号即可。
-    # candle_data['signal'] = pd.NaT
     # long
     c1 = candle_data['close'] > candle_data['upper']
     c2 = candle_data['close'].shift(1) < candle_data['upper']
@@ -130,11 +135,24 @@ def get_brin_signal(candle_data, period_interval=100, std_times=2):
     return signal_trade[candle_data.iat[-1, -1]]  # 只要最后一根K线的信号
 
 
-def place_order(exchange, symbol, amount, price, side):
+def okex_time_trans(time):
+    """时间格式转换
+
+    OKEX交易所API返回的UTC0的string型时间，如'2020-03-10T13:00:53.000Z'，
+    转换为string型的北京时间，且形式为'2020-03-10 21:00:53'
+
+    :param time: OKEX交易所API返回的UTC0的string型时间
+    :return: string型的北京时间
+    """
+    t = datetime.strptime(time[:-5], '%Y-%m-%dT%H:%M:%S') + timedelta(hours=8)
+    return datetime.strftime(t, '%Y-%m-%d %H:%M:%S')
+
+
+def okex_place_order(exchange, symbol, amount, price, side):
     """下单
 
-    只下限价单，不下市价单；只下“高级限价单”中的“全部成交或立即取消”（FillOrKill）
-    保证资金不被挂单占用。而且是以市价浮动2%的价格下单，成功成交概念大。
+    最多尝试5次，只下限价单，不下市价单；只下“高级限价单”中的“全部成交或立即取消”（FillOrKill）
+    保证资金不被挂单占用。而且是以市价浮动2%的价格下单，成功成交概率大。
 
     :param exchange: 交易所
     :param symbol: string; 交易币对，以“/”分隔，BTC/USDT
@@ -157,10 +175,120 @@ def place_order(exchange, symbol, amount, price, side):
                       'size': amount}
             order_info = exchange.margin_post_orders(params)
             if order_info['result']:
+                # 发送钉钉
+                params = {'instrument_id': symbol.replace('/', '-'), 'state': 2}
+                order_info = exchange.margin_get_orders(params)[0]   #取最新的成交订单
+                content = ''
+                content += '下单成交！\n'
+                content += '币对：' + order_info['instrument_id'] + '\n'
+                content += '类型：' + side + '\n'
+                content += '价格：' + order_info['price_avg'] + '\n'
+                content += '数量：' + order_info['filled_size'] + '\n'
+                content += '总额：' + order_info['filled_notional'] + '\n'
+                content += '时间：' + okex_time_trans(order_info['created_at']) + '\n'
+                content += '单号：' + order_info['order_id'] + '\n'
+                msg.send_dingding_msg(content)
+                # 返回下单结果
                 return order_info['order_id']
         except Exception as e:
-            time.sleep(1)  # 1秒后重试
+            print(e)
+            continue
+            # time.sleep(1)  # 1秒后重试
     return  # 否则，返回None
+
+
+def okex_borrow(exchange, symbol, coin, lever_times):
+    """借币，适用于OKEX交易所，币币杠杆账户
+
+    根据币币杠杆账户余额、以及允许的杠杆（交易所允许的杠杆倍数和个人设定的杠杆
+    倍数取最小值），来确定可借币的数量，并完成借币操作
+
+    :param exchange: 交易所
+    :param symbol: 交易币对
+    :param coin: 需要借的币
+    :param lever_times: 允许的最大杠杆倍数
+    :return:
+        borrow_id: 借币订单号
+        None: 借币失败
+    """
+    for i in range(5):
+        try:
+            # 获取杠杆配置信息
+            params = {'instrument_id': symbol.replace('/', '-')}
+            lever_info = exchange.margin_get_accounts_instrument_id_availability(params)
+            # 确定杠杆倍数
+            lever_time_available = float(lever_info[0]['currency:' + coin]['leverage'])
+            lever = min(lever_times, lever_time_available)  # lever int型变量
+            # 确定借币数量
+            f1 = float(lever_info[0]['currency:' + coin]['available'])          # 最大可借币数
+            f2 = lever_time_available - 1.0                                     # 最大可借杠杆倍数（-1为本金数量为1倍）
+            borrow_amount = int(f1 / f2 * (lever - 1) * 100000000) / 100000000  # 取整小数点后前8位，避免数值过长出错
+            # 借币
+            params = {'instrument_id': symbol.replace('/', '-'), 'currency': coin, 'amount': str(borrow_amount)}
+            borrowed_info = exchange.margin_post_accounts_borrow(params)
+            if not borrowed_info['result']:  # 如果借币失败，那么…………
+                content = 'borrowed failed! Can not borrowed from exchange!'
+                msg.send_dingding_msg(content)
+                return
+            # 发送钉钉借币成功消息
+            params = {'instrument_id': symbol.replace('/', '-'), 'state': 0}  # 获取借币信息
+            info = exchange.margin_get_accounts_instrument_id_borrowed(params)
+            content = '借币成功！\n'
+            content += '币种：' + info[0]['currency'] + '\n'
+            content += '数量：' + info[0]['amount'] + '\n'
+            content += '币对：' + info[0]['instrument_id'] + '\n'
+            content += '时间：' + okex_time_trans(info[0]['timestamp']) + '\n'
+            content += '单号：' + info[0]['borrow_id'] + '\n'
+            content += '限时：' + okex_time_trans(info[0]['force_repay_time']) + '\n'
+            content += '利率：' + info[0]['rate'] + '\n'
+            msg.send_dingding_msg(content)
+            # 返回结果
+            return borrowed_info['borrow_id']
+        except Exception as e:
+            if i == 4:
+                content = '借币报错次数达到5次，失败！请检查代码或网络。'
+                msg.send_dingding_msg(content)
+            print(e)
+            continue
+    return
+
+
+def okex_repayment(exchange, symbol, coin):
+    """还币，适用于OKEX交易所，币币杠杆账户，且只还最近的一笔未还清借款。
+
+    :param exchange: 交易所
+    :param symbol: 交易币对，以‘/’连接，例“BTC/USDT”
+    :param coin: 需要还的币种
+
+    :return:
+        repayment_id: str，订单号；
+        exit: 任意原因导致还款失败，则退出程序。
+    """
+
+    try:
+        params = {'instrument_id': symbol.replace('/', '-'), 'state': 0}  # 获取借币信息
+        info = exchange.margin_get_accounts_instrument_id_borrowed(params)
+        info_balance = exchange.fetch_balance({'type': 'margin'})[symbol]
+        f = float(info[0]['amount']) + float(info[0]['interest'])  # 还币，成功前提：只有一笔未还借款。
+        amount = ceil(f * 100000000) / 100000000     # 保留小点后8位，进一
+        if float(info_balance[coin]['free']) < amount:
+            content = '账户余额不足，无法还币，请及时补充余额减少利息！程序退出！'
+            msg.send_dingding_msg(content)
+            exit()
+        params = {'instrument_id': symbol.replace('/', '-'), 'currency': coin, 'amount': amount}
+        info_repay = exchange.margin_post_accounts_repayment(params)    # 还币
+        if info_repay['result']:
+            return info_repay['repayment_id']
+        elif not info_repay['result']:
+            content = '还款结果返回False，还款不成功，程序退出。请及时还币减少利息，再行启动策略程序！'
+            msg.send_dingding_msg(content)
+            exit()
+        return
+    except Exception as e:
+        content = 'OKEX交易所还币函数异常，请及时还币后，再行启动策略程序。'
+        msg.send_dingding_msg(content)
+        print(e)
+        exit()
 
 
 # --------助教，下面两个函数没有使用----------
--- main.py
+++ main.py
@@ -8,20 +8,21 @@ from setting import Settings
 my_settings = Settings()
 
 myOKEX = ccxt.okex3()
-myOKEX.apiKey = ''
-myOKEX.secret = ''
-myOKEX.password = ''
-myOKEX.proxies = {'http': 'socks5h://127.0.0.1:1080',
-                  'https': 'socks5h://127.0.0.1:1080'}
+myOKEX.apiKey = my_settings.okex_entry['apikey']
+myOKEX.secret = my_settings.okex_entry['secret']
+myOKEX.password = my_settings.okex_entry['password']
+myOKEX.proxies = my_settings.proxies
 
 # 初始化参数
-symbol = 'BTC/USDT'
+symbol = 'BTC/USDT'     # ccxt采用“/”连接币对，OKEX采用“-”连接
 trade_coin = symbol.split('/')[0]
 base_coin = symbol.split('/')[-1]
 time_interval = '15m'   # 确定获取哪个时间周期的K线
+
+# ----有问题------------------有问题--------------有问题--------------有问题
 signal_last = 'closing'  # 上一次交易类型，初始交易状态为平仓
-buy_or_sell = ''
-order_id = ''
+# ----有问题------------------有问题--------------有问题--------------有问题
+
 # 开始主循环
 while True:
     # 1、取得并等到获取数据的时间点
@@ -39,59 +40,86 @@ while True:
 
     # 3.根据最新数据计算买卖信号
     signal_trade = fs.get_brin_signal(candle_data)
+
+    # 删除部分------------------------------------------------------------------------------------
+    print(signal_trade)
+    # 删除部分------------------------------------------------------------------------------------
+
     if signal_trade == 'nothing': continue
 
-    # 根据signal_trade和上一次交易类型，确定下买单或下卖单
-    signal_verification = {'long': {'long': 'nothing', 'short': 'buy', 'closing': 'buy'},
-                           'short': {'long': 'sell', 'short': 'nothing', 'closing': 'sell'},
-                           'closing': {'long': 'sell', 'short': 'buy', 'closing': 'nothing'}}
-    buy_or_sell = signal_verification[signal_trade][signal_last]
-    if buy_or_sell == 'nothing': continue
-
     # 4.下单
-    # 4.1 获取账户余额信息，市场ticker信息，借币信息
+    # 4.1 获取账户余额信息，市场ticker信息
     try:
         balance_info = myOKEX.fetch_balance({'type': 'margin'})[symbol]     # 币币杠杆户交易币对余额
         ticker_info = myOKEX.fetch_ticker(symbol)
     except Exception as e:
         print('Can not get account balance and tiker.')
         continue
-    # 4.2 下买单、或下卖单
-    if buy_or_sell == 'buy':
-        if balance_info[base_coin]['free'] > 0:
-            price = ticker_info['ask'] * 1.02
-            amount = balance_info[base_coin]['free'] / price
-            order_id = fs.place_order(myOKEX, symbol, amount, price, buy_or_sell)
-        else:
-            msg.send_dingding_msg('下买单失败，账户余额不足！')
-            continue
-    elif buy_or_sell == 'sell':
-        if balance_info[trade_coin]['free'] > 0:
-            price = ticker_info['bid'] * 0.98
-            amount = balance_info[trade_coin]['free']
-            order_id = fs.place_order(myOKEX, symbol, amount, price, buy_or_sell)
-        else:
-            msg.send_dingding_msg('下卖单失败，账户余额不足！')
-            continue
+    # 4.2 交易信号组合，共6种（本次交易信号只会有三种，上一次交易也只会有三种）
+    # 第一种情况：平空仓，开多仓
+    if signal_trade == 'long' and signal_last == 'short':
+        price = ticker_info['ask'] * 1.02
+        # 01\平掉空仓，下买单
+        amount = int(float(balance_info[base_coin]['free']) / price * 10**8) / 10**8
+        order_id = fs.okex_place_order(myOKEX, symbol, amount, price, 'buy')
+        if order_id is None: continue
+        # 02\还币（BTC）
+        fs.okex_repayment(myOKEX, symbol, trade_coin)
+        # 03\借币（USDT）
+        fs.okex_borrow(myOKEX, symbol, base_coin, my_settings.lever_times)
+        # 04\加杠杆，下买单
+        balance_info = myOKEX.fetch_balance({'type': 'margin'})[symbol]
+        amount = int(float(balance_info[base_coin]['free']) / price * 10**8) / 10**8
+        fs.okex_place_order(myOKEX, symbol, amount, price, 'buy')
+    # 第二种情况：开多仓。之前的平仓分两种情况，平多仓和平空仓，不需区分情况写代码，用以下代码结果一样。
+    elif signal_trade == 'long' and signal_last == 'closing':
+        # 01\借币(USDT)
+        fs.okex_borrow(myOKEX, symbol, base_coin, my_settings.lever_times)
+        # 02\加杠杆，下买单
+        price = ticker_info['ask'] * 1.02
+        balance_info = myOKEX.fetch_balance({'type': 'margin'})[symbol]
+        amount = int(float(balance_info[base_coin]['free']) / price * 10**8) / 10**8
+        fs.okex_place_order(myOKEX, symbol, amount, price, 'buy')
+    # 第三种情况：平多仓，开空仓
+    elif signal_trade == 'short' and signal_last == 'long':
+        price = float(ticker_info['bid']) * 0.98
+        # 01\平多仓，下卖单
+        amount = float(balance_info[trade_coin]['free'])
+        fs.okex_place_order(myOKEX, symbol, amount, price, 'sell')
+        # 02\还币(USDT)
+        fs.okex_repayment(myOKEX, symbol, base_coin)
+        # 03\借币(BTC)
+        fs.okex_borrow(myOKEX, symbol, trade_coin, my_settings.lever_times)
+        # 04\加杠杆，下卖单
+        balance_info = myOKEX.fetch_balance({'type': 'margin'})[symbol]
+        amount = float(balance_info[trade_coin]['free'])
+        fs.okex_place_order(myOKEX, symbol, amount, price, 'sell')
+    # 第四种情况：开空仓
+    elif signal_trade == 'short' and signal_last == 'closing':
+        # 01\借币(BTC)
+        fs.okex_borrow(myOKEX, symbol, trade_coin, my_settings.lever_times)
+        # 02\加杠杆，下卖单
+        price = ticker_info['bid'] * 0.98
+        balance_info = myOKEX.fetch_balance({'type': 'margin'})[symbol]
+        amount = float(balance_info[trade_coin]['free'])
+        fs.okex_place_order(myOKEX, symbol, amount, price, 'sell')
+    # 第五种情况：平多仓
+    elif signal_trade == 'closing' and signal_last == 'long':
+        # 01\下卖单
+        price = ticker_info['bid'] * 0.98
+        amount = float(balance_info[trade_coin]['free'])
+        fs.okex_place_order(myOKEX, symbol, amount, price, 'sell')
+        # 02\还币(USDT)
+        fs.okex_repayment(myOKEX, symbol, base_coin)
+    # 第六种情况：平空仓
+    elif signal_trade == 'closing' and signal_last == 'short':
+        # 01\下买单
+        price = ticker_info['ask'] * 1.02
+        amount = int(float(balance_info[base_coin]['free']) / price * 10**8) / 10**8
+        fs.okex_place_order(myOKEX, symbol, amount, price, 'buy')
+        # 02\还币(BTC)
+        fs.okex_repayment(myOKEX, symbol, trade_coin)
+    else: continue
 
-    if not order_id is None:    # 如果下单成功，那么……
-        sleep(10)   # 延时，
-        # 发送钉钉
-        params = {'instrument_id': symbol.replace('/', '-'), 'state': 2}
-        order_info = myOKEX.margin_get_orders(params)[0]   #取最新的成交订单
-        content = ''
-        content += '下单成交！\n'
-        content += '币对：' + order_info['instrument_id'] + '\n'
-        content += '类型：' + buy_or_sell + '\n'
-        content += '价格：' + order_info['price_avg'] + '\n'
-        content += '数量：' + order_info['filled_size'] + '\n'
-        content += '总额：' + order_info['filled_notional'] + '\n'
-        content += '杠杆：' + '1' + '\n'
-        content += '时间：' + order_info['created_at'] + '\n'
-        content += '单号：' + order_info['order_id'] + '\n'
-        msg.send_dingding_msg(content)
-
-        signal_last = signal_trade
-        signal_trade = 'nothing'
-        buy_or_sell = ''
-        order_id = ''
+    signal_last = signal_trade
+    signal_trade = 'nothing'
